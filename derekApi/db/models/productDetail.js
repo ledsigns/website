812501501320d94025b6f33a9c75874796f72302
Vinny <10vmarcos342@gmail.com>
2019-04-20 14:39:03 +0800

    Added Click schema to Product Detail. NNo further

--- derekApi/db/models/productDetail.js
+++ derekApi/db/models/productDetail.js
@@ -1,20 +1,68 @@
-const mongoose = require('./init').mongoose
+const mongoose = require("./init").mongoose;
+
+// ProductDetail = ({
+//      productId: [
+// {
+//     userID: [
+//         //For registered user
+//         {id: '799677146374973'},
+//         {id: '098y4706'},
+//         // For anonymous
+//         {id: null},
+//         {id: null},
+//         {id: null},
+//         {id: null},
+//     ]
+// }
+//   ]
+// });
+
+// ProductDetail = ({
+//     images: [
+//         {
+//             link: String,
+//             description: String
+//         }
+//     ],
+//     productIntro: String,
+//     ...,
+//     clicks: ClickSchema,
+// });
+
+const ClickSchema = mongoose.Schema({
+  productId: [
+    {
+      User: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        // required: true,
+        default: null
+      },
+      description: String
+    }
+  ]
+});
 
 const ProductDetailSchema = mongoose.Schema({
-    images: [{
-        link: String,
-        description: String
-    }],
-    productIntro: String,
-    specs: String,
-    showCase: [{
-        link1: String,
-        link2: String,
-        link3: String,
-        description: String
-    }],
-})
+  images: [
+    {
+      link: String,
+      description: String
+    }
+  ],
+  productIntro: String,
+  specs: String,
+  showCase: [
+    {
+      link1: String,
+      link2: String,
+      link3: String,
+      description: String
+    }
+  ],
+  clicks: ClickSchema
+});
 
-const ProductDetail = mongoose.model('ProductDetail', ProductDetailSchema)
+const ProductDetail = mongoose.model("ProductDetail", ProductDetailSchema);
 
-module.exports = ProductDetail
+module.exports = ProductDetail;
